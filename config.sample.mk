fb2b8e986677a715ce785474467279115ff51b54
sttachoires <stephane.tachoires@gmail.com>
2016-02-24 19:58:18 +0100

    Makefile up

---
+++ config.sample.mk
@@ -0,0 +1,16 @@
+NAME = pgm
+VERSION = 0.01-dev
+
+# Customize below to fit your system
+
+# Bash is mandatory
+BASH = /bin/bash
+
+# paths
+USER = postgres
+PREFIX = /home/pgm
+BINDIR = ${PREFIX}/bin
+LIBDIR = ${PREFIX}/lib/${NAME}
+SYSCONFDIR = ${PREFIX}/etc/${NAME}
+DOCDIR = ${PREFIX}/share/doc/${NAME}
+MANDIR = ${PREFIX}/share/man
